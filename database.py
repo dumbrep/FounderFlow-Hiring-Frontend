1f67e6709cc9ebef31edd66cdc192dba53c6d634
dumbrep <dumbreprajwal@gmail.com>
2026-04-23 03:21:27 +0530

    added .vercel

--- database.py
+++ database.py
@@ -1,7 +1,9 @@
 from motor.motor_asyncio import AsyncIOMotorClient
 import os
 
-MONGO_URL = os.getenv("MONGO_URL", "mongodb+srv://prajwaldumbre_db_user:Prajwal123@cluster0.er5qxvd.mongodb.net/?appName=Cluster0")
+MONGO_URL = os.getenv("MONGO_URL")
+if not MONGO_URL:
+    raise RuntimeError("MONGO_URL environment variable is not set")
 DB_NAME = os.getenv("DB_NAME", "hiring_deployments")
 
 client = AsyncIOMotorClient(MONGO_URL)
